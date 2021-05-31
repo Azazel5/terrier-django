123eca5044077b2b7f98564464c46903e072e882
Azazel5 <subhanga2013@gmail.com>
2021-05-30 20:46:19 -0400

    Postgres connection and csv reading done

---
+++ scheduler/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+
+class Technician(models.Model):
+    technician_name = models.CharField(max_length=255)
+
+class WorkOrder(models.Model):
+    work_order_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    work_order_technician = models.ForeignKey(Technician, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    duration = models.IntegerField()
+    price = models.IntegerField()
---
+++ scheduler/views.py
@@ -0,0 +1,93 @@
+import csv
+from datetime import datetime
+from django.shortcuts import render
+from terrier.settings import BASE_DIR 
+from datetime import datetime, timedelta
+from .models import Location, Technician, WorkOrder
+
+def daterange(start_date, end_date):
+    delta = timedelta(hours=1)
+    while start_date < end_date:
+        yield start_date
+        start_date += delta
+
+def home_view(request):
+    load_csvs()
+    work_orders = WorkOrder.objects.all()
+    time_list = []
+
+    start_date = datetime(2013, 1, 1, 00, 00)
+    end_date = datetime(2013, 1, 1, 23, 59)
+    time_list = [single_date.strftime("%H:%M") for single_date in daterange(start_date, end_date)]
+    
+
+    context = {
+        "work_orders": work_orders,
+        "technicians": Technician.objects.all(),
+        "time_list": time_list
+    } 
+
+    return render(request, template_name="home.html", context=context)
+
+def load_csvs():
+    locations_file = BASE_DIR / 'scheduler' / 'csvs' / 'locations.csv'
+    technicians_file = BASE_DIR / 'scheduler' / 'csvs' / 'technicians.csv'
+    work_orders_file = BASE_DIR / 'scheduler' / 'csvs' / 'work_orders.csv'
+
+    with open(locations_file) as csv_locations:
+        csvreader = csv.reader(csv_locations)
+        _ = next(csvreader)
+
+        for row in csvreader:
+            _, created = Location.objects.get_or_create(
+                location_name=row[1],
+                city=row[2]
+            )
+
+            if created:
+                print("{} - {} inserted to the database".format(row[1], row[2]))
+
+    with open(technicians_file) as csv_technicians:
+        csvreader = csv.reader(csv_technicians)
+        _ = next(csvreader)
+
+        for row in csvreader:
+            _, created = Technician.objects.get_or_create(
+                technician_name=row[1],
+            )
+
+            if created:
+                print("{} inserted to the database".format(row[1]))
+
+
+    with open(work_orders_file) as csv_work:
+        csvreader = csv.reader(csv_work)
+        _ = next(csvreader)
+        locations = Location.objects.all()
+        technicians = Technician.objects.all()
+
+        for row in csvreader:
+            technician = technicians[int(row[1]) - 1]
+            location = locations[int(row[2]) - 1]
+            row[3] = row[3][:5] + "20" + row[3][5:] 
+            original_datetime = datetime.strptime(row[3], "%m/%d/%Y %H:%M")
+            converted_date_time = datetime.strftime(original_datetime, "%Y-%m-%d %H:%M") 
+
+            _, created = WorkOrder.objects.get_or_create(
+                work_order_location=location,
+                work_order_technician=technician,
+                time=converted_date_time,
+                duration=row[4],
+                price=row[5]
+            )
+
+            if created:
+                print("Work order created for {} at {}".format(technician.technician_name, row[3]))
+
+            
+
+
+    
+
+
+
